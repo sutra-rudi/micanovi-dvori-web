ed6caa52d8be0345b3f4c25b13aa42a81139bf13
sutra-rudi <rudi@sutra.hr>
2024-04-22 09:39:39 +0200

    setting up the project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,38 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import AppHeader from './components/AppHeader';
+import styles from './styles/page.module.scss';
+import { Suspense } from 'react';
+import Loading from './loading';
+import AppFooter from './components/AppFooter';
+// import HeroSekcija from './sections/HeroSekcija';
+// import OdmoriSeSekcija from './sections/OdmoriSeSekcija';
+// import KampKuciceSekcija from './sections/KampKuciceSekcija';
+// import ExploreCampSection from './sections/ExploreCampSection';
+// import ParallaxVideoSection from './sections/ParallaxVideoSection';
+// import FAQsection from './sections/FAQsection';
+// import DodatneInformacije from './sections/DodatneInformacije';
+// import OnamaSekcija from './sections/OnamaSekcija';
+// import PogledajVideo from './sections/PogledajVideo';
+// import GallerySection from './sections/GallerySection';
+// import ReviewsSection from './sections/ReviewsSection';
 
-export default function Home() {
+export default async function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main className={styles.homeMain}>
+      <AppHeader />
+      <Suspense fallback={<Loading />}>
+        {/* <HeroSekcija />
+        <OdmoriSeSekcija />
+        <KampKuciceSekcija />
+        <ExploreCampSection />
+        <ParallaxVideoSection />
+        <ReviewsSection />
+        <PogledajVideo />
+        <OnamaSekcija />
+        <DodatneInformacije isLanding />
+        <FAQsection />
+        <GallerySection /> */}
+      </Suspense>
+      <AppFooter />
     </main>
   );
 }
